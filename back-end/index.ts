0ba904b2c30ebce04466eae993121587e69c08db
dsmathers <dsmathers91@gmail.com>
2022-02-03 16:29:28 -0800

    added some folder structure and added a test route, all good so far.

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,21 +1,28 @@
 import express from 'express';
-import cors from 'cors'
-import mongoose from 'mongoose'
+import cors from 'cors';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+//Import Routes
 
+import getRoutes from './routes/gets/testGet'
 
+dotenv.config()
 const app = express();
  app.use(cors());
  app.use(bodyParser.json({limit: '120kb'}));
  app.use(bodyParser.urlencoded({limit: '120kb', extended: true}));
 
-
- app.get('/', (req, res) => {
+ /* app.get('/', (req, res) => {
      res.send('Hello World');
      console.log('Test Success');
- })
+ }) */
+
+ app.use('/', getRoutes);
 
  let PORT = process.env.PORT || 8000;
+
+ export default PORT;
 
  
 
