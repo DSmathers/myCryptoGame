2d9d23267d4489286f1c773a461bcb387398bfa1
dsmathers <dsmathers91@gmail.com>
2022-02-05 20:36:54 -0800

    .

--- back-end/services/database/addNewUser.ts
+++ back-end/services/database/addNewUser.ts
@@ -4,8 +4,8 @@ import User from '../../models/UserWallet'
 
 
 async function addNewUser(uid:string) {
-    const connectionURL = process.env.DB_CONNECTION_URL;
     let newUser = new User({uid:uid, usd: 10000})
+    const connectionURL = process.env.DB_CONNECTION_URL;
     mongoose.connect(connectionURL?connectionURL:'').catch(error => console.log(error))
     await newUser.save()
     .catch((error) => {return error.message})
