d5ad6ec11d742c0f155f0d34d47ef6a7c9887fe6
dsmathers <dsmathers91@gmail.com>
2022-02-08 19:05:45 -0800

    some work on coin description modal

--- front-end/src/Views/Game/market/components/CoinDetailsModal.tsx
+++ front-end/src/Views/Game/market/components/CoinDetailsModal.tsx
@@ -1,17 +1,62 @@
+import axios from 'axios';
 import { Modal } from 'react-bootstrap'
+import { useState } from 'react'
 
 
 const CoinDetailsModal = ({ selectedCoin, setSelectedCoin }:any) => {
-   
-    const handleClose = () => {
-        setSelectedCoin(null)
-    }
+   const [ coinData, setCoinData ]: null | any = useState(null);
+  
+    const getCoinDetails = async() => {
+        await axios.get('https://api.coingecko.com/api/v3/coins/'+selectedCoin).then((res) => {
+            if(res.status === 200){
+                setCoinData(res.data);  
+                console.log(res.data.market_data)
+            }
+            else {throw new Error(res.statusText)}
+            
+        }).catch((error) => {
+            throw new Error(error)
+        });
+    };
+
+    // Note to self: This is the text description of the selected cryptocurrency. I needed
+    // to use (i think) a weird method (to me) to make the <a> tags actually render... 
+    const parser = new DOMParser();
+    let parsed = parser.parseFromString(coinData?.description.en, "text/html")
+    let justHoldingThisHere = <p dangerouslySetInnerHTML={{ __html: parsed.body.innerHTML}}></p>
 
   return (
-      <Modal show={selectedCoin?true:false}>
-          <Modal.Header> Modal Header <button onClick={handleClose}>Close</button> </Modal.Header>
-          <Modal.Body> {selectedCoin} </Modal.Body>
-      </Modal>
+    <Modal show={selectedCoin?true:false} fullscreen='lg-down' centered onShow={()=>{getCoinDetails()}} onHide={()=>{setSelectedCoin(null)}}>
+        <Modal.Header closeButton>
+        
+            {coinData? 
+            <div>
+                <p>{coinData?.name} price</p>
+                <p>${coinData?.market_data.current_price.usd.toLocaleString()}</p>
+                <p>${coinData?.market_data.price_change_24h_in_currency.usd.toLocaleString()} ({coinData.market_data.price_change_percentage_24h_in_currency.usd.toFixed(2)}%)</p>
+            </div>:<></>}
+        </Modal.Header>
+        <Modal.Body style={{overflowY: 'auto'}}>
+            <div>
+                // Price Chart Box / Div Goes Here //
+            </div>
+            <div> // Market Data Section Goes Here // 
+            {/* Market Data Values
+                TODO: Decide which values to add for initial launch and make it pretty.
+                can always add more later.  */}
+            
+            </div>
+            <div>
+                {/* The parsed html string mentioned above.(Text details about selected currency) */}
+                {justHoldingThisHere}    
+            </div>
+            
+
+        </Modal.Body>
+        <Modal.Footer>
+            // Buy / Sell / Trade Links Go Here //
+        </Modal.Footer>
+    </Modal>
   );
 };
 
