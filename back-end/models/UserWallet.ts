7e77bfa4a798bb2906e674caa037abe12ce00d40
dsmathers <dsmathers91@gmail.com>
2022-02-14 20:45:41 -0800

    updated user model; added watchlist array and transaction history array

--- back-end/models/UserWallet.ts
+++ back-end/models/UserWallet.ts
@@ -1,21 +1,54 @@
-import mongoose from 'mongoose'
+import mongoose, { Date } from 'mongoose'
 
-interface User {
+
+interface newUser {
     uid: string,
-    // Supported Coins Goes Here...
-    usd: number,
-    btc?: number,
-    eth?: number,
-    algo?: number
-}
+    wallet: {
+        usd: number,
+        btc?: number,
+        eth?: number,
+        algo?: number
+    },
+    watchlist?: [string],
+    transactions?: [{
+        date: Date,
+        summary: {
+            out_asset_name: string,
+            out_asset_total: number,
+            in_asset_name: string,
+            in_asset_total: number
+        }
+    }]
+};
 
-const walletSchema = new mongoose.Schema<User>({
+
+const userSchema = new mongoose.Schema<newUser>({
     uid: {type: String, required: true},
-    usd: {type: Number, required: false},
-    btc: {type: Number, required: false},
-    eth: {type: Number, required: false}
+    wallet: {
+        usd: {type: Number, required: false},
+        btc: {type: Number, required: false},
+        eth: {type: Number, required: false},
+        usdt: {type: Number, required: false},
+        bnb: {type: Number, required: false},
+        usdc: {type: Number, required: false},
+        xrp: {type: Number, required: false},
+        ada: {type: Number, required: false},
+        sol: {type: Number, required: false},
+        dot: {type: Number, required: false},
+    },
+    watchlist: [{type: String, required: false}],
+    transactions: [{
+        date: {type: Date, required: true},
+        summary: {
+            out_asset_id: {type: String, required: true},
+            out_asset_total: {type: Number, required: true},
+            in_asset_id: {type: String, required: true},
+            in_asset_total: {type: Number, required: true}
+        }
+    }]
 })
 
-const User = mongoose.model('Wallets', walletSchema);
+
+const User = mongoose.model('Users', userSchema);
 
 export default User
